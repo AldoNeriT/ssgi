8c04952070137a115dc63b3b28e30760f1392dce
Aldo Neri <aldo_neri@outlook.com>
2019-10-15 22:02:41 -0500

    Avance en Lista de Verificacion

--- src/app/pages/lista-verificacion/lista-verificacion.component.ts
+++ src/app/pages/lista-verificacion/lista-verificacion.component.ts
@@ -1,5 +1,16 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormControl, Validators } from '@angular/forms';
+import { PlaneacionService, UsuarioService, AuditoriaService } from '../../services/service.index';
+import { Planeacion } from '../../models/planeacion.model';
+import { Institucion } from '../../models/institucion.model';
 import { Router, ActivatedRoute } from '@angular/router';
+
+import * as pdfMake from 'pdfmake/build/pdfmake.js';
+import * as pdfFonts from 'pdfmake/build/vfs_fonts.js';
+pdfMake.vfs = pdfFonts.pdfMake.vfs;
+
+import Swal from 'sweetalert2';
+import * as $ from 'jquery';
 
 @Component({
   selector: 'app-lista-verificacion',
@@ -8,20 +19,87 @@ import { Router, ActivatedRoute } from '@angular/router';
 })
 export class ListaVerificacionComponent implements OnInit {
 
-  // id: string;
+  idP: string;
+  idU: string;
 
-  constructor( public router: Router,
+  planeacion: any[] = [];
+
+  arrProActCriV: any[] = [];
+
+  cargando = true;
+
+  procesoV: string;
+  auditoriaV: string;
+  normasV: string;
+  auditorV: string;
+
+  constructor( public _planeacionService: PlaneacionService,
+               public _usuarioService: UsuarioService,
+               public _auditoriaService: AuditoriaService,
+               public router: Router,
                public activatedRoute: ActivatedRoute ) {
-    // activatedRoute.params.subscribe( params => {
-    //   this.id = params['idP'];
-    // });
+    activatedRoute.params.subscribe( params => {
+      this.idP = params['idP'];
+      this.idU = params['idU'];
+    });
   }
 
   ngOnInit() {
+    this.cargarPlaneacion( this.idP );
   }
 
   regresar( idAuditoria: string ) {
     this.router.navigate(['/listas/' + idAuditoria]);
+  }
+
+  cargarPlaneacion( id: string ) {
+    this.cargando = true;
+
+    this._planeacionService.cargarPlaneacion( id )
+          .subscribe( planeaciones => {
+            this.planeacion = planeaciones;
+            console.log('Planeacion: ', this.planeacion);
+
+            this.auditoriaV = planeaciones.auditoria.nombreAuditoria;
+            this.auditorV = this._usuarioService.usuario.nombre + ' ' + this._usuarioService.usuario.primer_Apellido + ' ' + this._usuarioService.usuario.segundo_Apellido || ''
+
+            let array: any[] = [];
+            if ( planeaciones.proceso.nombreProceso ) {
+              array.push(planeaciones.proceso.nombreProceso);
+            }
+            if ( planeaciones.actividad ) {
+              array.push(planeaciones.actividad);
+            }
+            if ( planeaciones.criterio ) {
+              array.push(planeaciones.criterio);
+            }
+            if ( planeaciones.area ) {
+              array.push(planeaciones.area);
+            }
+
+            this.arrProActCriV.push(array.join(', '));
+
+
+            this._auditoriaService.cargarAuditoria( planeaciones.auditoria._id )
+                .subscribe( auditoria => {
+                  // this.institucion = instituciones.instituciones;
+                  console.log('Auditoria: ', auditoria);
+
+
+                  let arrNormasV: any[] = [];
+                  for ( let nor of  auditoria.normas) {
+                    arrNormasV.push(nor.nombreNorma);
+                  }
+
+                  this.normasV = arrNormasV.join(', ');
+
+
+                  this.cargando = false;
+                });
+
+
+            this.cargando = false;
+          });
   }
 
 }
