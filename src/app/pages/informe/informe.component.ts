85548d7c8168da1bb8f289fc04b94b4182a6cd95
Aldo Neri <aldo_neri@outlook.com>
2019-10-18 12:34:11 -0500

    Se está creando el diseño del Informe

--- src/app/pages/informe/informe.component.ts
+++ src/app/pages/informe/informe.component.ts
@@ -1,5 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 
+declare function floating_labels();
+declare function inicializando_datePicker();
+
 @Component({
   selector: 'app-informe',
   templateUrl: './informe.component.html',
@@ -7,9 +10,28 @@ import { Component, OnInit } from '@angular/core';
 })
 export class InformeComponent implements OnInit {
 
+  mostrarFormTitulo = false;
+  mostrarFormCom = false;
+
   constructor() { }
 
   ngOnInit() {
+    floating_labels();
+    inicializando_datePicker();
+  }
+
+  mFormTitulo() {
+    this.mostrarFormTitulo = !this.mostrarFormTitulo;
+
+    floating_labels();
+    inicializando_datePicker();
+  }
+
+  mFormCom() {
+    this.mostrarFormCom = !this.mostrarFormCom;
+
+    floating_labels();
+    inicializando_datePicker();
   }
 
 }
